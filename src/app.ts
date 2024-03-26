f0b9876c90562ca0aa1796a0b2a28becd8487cb1
Gurudatt SA <>
2024-03-26 22:25:51 +0530

    feat: prettier

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,39 @@
-import { config } from "../utils/config"
-import { createServer } from "../utils/createServer"
-import { connectToDB, disconnectFromDB } from "../utils/db"
-import { logger } from "../utils/logger"
+import { config } from '../utils/config';
+import { createServer } from '../utils/createServer';
+import { connectToDB, disconnectFromDB } from '../utils/db';
+import { logger } from '../utils/logger';
 
-const signals = ["SIGINT", "SIGTERM", "SIGHUP"] as const
+const signals = ['SIGINT', 'SIGTERM', 'SIGHUP'] as const;
 
 async function gracefullShutdown({
   signal,
-  server
+  server,
 }: {
-  signal: (typeof signals)[number]
-  server: Awaited<ReturnType<typeof createServer>>
+  signal: (typeof signals)[number];
+  server: Awaited<ReturnType<typeof createServer>>;
 }) {
-  logger.info(`Got signal ${signal}. Shutting down.`)
-  await server.close()
-  await disconnectFromDB()
-  process.exit(0)
+  logger.info(`Got signal ${signal}. Shutting down.`);
+  await server.close();
+  await disconnectFromDB();
+  process.exit(0);
 }
 
 async function startServer() {
-  const server = await createServer()
+  const server = await createServer();
 
   server.listen({
     port: config.PORT,
-    host: config.HOST
-  })
+    host: config.HOST,
+  });
 
-  await connectToDB()
-  logger.info(`App is listening`)
+  await connectToDB();
+  logger.info(`App is listening`);
 
   for (let i = 0; i < signals.length; i++) {
     process.on(signals[i], () => {
-      gracefullShutdown({ signal: signals[i], server })
-    })
+      gracefullShutdown({ signal: signals[i], server });
+    });
   }
 }
 
-startServer()
+startServer();
